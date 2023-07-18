a19c74ff0665688e2d7a2848c8926f5e9e8b8ecc
FinviaBot <77678032+FinviaBot@users.noreply.github.com>
2023-07-18 20:58:07 +0200

    Simplify entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,6 @@
 set -exo pipefail
 
 update-ca-certificates
-wait-service --tcp $AWAITED_ENDPOINT -- $@
+wait-service --tcp $AWAITED_ENDPOINT -- echo "'$AWAITED_ENDPOINT' is up. Booting..."
+
+exec "$@"
